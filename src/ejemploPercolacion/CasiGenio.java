08911842bd53fd44ffa535e584d20d0a9e359ba8
Daniel <danov54@estudiantec.cr>
2021-03-20 18:18:54 -0600

    Modified text in code

--- src/ejemploPercolacion/CasiGenio.java
+++ src/ejemploPercolacion/CasiGenio.java
@@ -3,7 +3,7 @@ package ejemploPercolacion;
 public class CasiGenio {
 	public static int coeficienteIntelectual(int coeficienteIntelectual) throws Exception {
 		if (coeficienteIntelectual < 120 || coeficienteIntelectual> 129) {
-			throw new Exception("su iq o es muy bajo o muy alto para considerarse casi un genio");
+			throw new Exception("su IQ o es muy bajo o muy alto para considerarse casi un genio");
 		}
 		return coeficienteIntelectual;
 	}
--- src/ejemploPercolacion/Main.java
+++ src/ejemploPercolacion/Main.java
@@ -4,6 +4,6 @@ public class Main {//Percolación de la excepción
 
 	public static void main(String[] args) throws Exception {
 		CasiGenio.coeficienteIntelectual(110);
-		System.out.println("Análisis finalizado");
+		System.out.println("Análisis completado");
 	}
 }
